a6f05112ab6e6bfc1cf3f9ab721690c5fc26778a
Jose E S Pimentel <jespimentel@uol.com.br>
2023-04-14 20:48:44 -0300

    Evolução
    Criação de função para retornar o resultado das pesquisas

--- main.py
+++ main.py
@@ -42,9 +42,24 @@ def consulta_num_oab(numero_oab):
 
     return response
 
+def faz_busca(soup):
+    mensagem = soup.find('div', {'id': 'spwTabelaMensagem'})
+    if mensagem is not None:
+        texto = mensagem.find('td', {'id':'mensagemRetorno'})
+        texto = texto.find('li').get_text()
+        return texto
+    
+    relacao_processos = soup.find('div', {'id': 'listagemDeProcessos'})
+    if relacao_processos is not None:
+        return('Encontrei uma lista de processos')
+         
+    processo_unico = soup.find('div', {'class': 'container'})
+    if processo_unico is not None:
+         return('Encontrei uma lista de processos')
+
+
 # Verifica a existência dos arquivos "investigados.txt" e "oab.txt".
 # Caso não existam, sai do programa
-import os.path
 condicao = os.path.exists('config_pesquisa/investigados.txt') and os.path.exists('config_pesquisa/oab.txt')
 if(not(condicao)):
   sys.exit()
@@ -63,85 +78,65 @@ with open('config_pesquisa/oab.txt', 'r') as f:
     for registro in f:
         oab.append(registro.strip())
 
-print(investigados)
-print(oab)
-
 # Cria o arquivo txt para gravar a pesquisa (com a data/hora)
 now = datetime.datetime.now()
-now_formatado = now.strftime('%Y-%m-%d-%H-%m')
-now_legivel = now.strftime('%d/%m/%Y - %Hh%mmin')
-arquivo_resultado = 'resultado-' + now_formatado +'.txt'
-with open(arquivo_resultado, 'w') as f:
-    f.write(f'RESULTADO DA VARREDURA REALIZADA EM {now_legivel}')
-    f.write('\n' + '-'* 58 + '\n\n')
+now_formatado = now.strftime('%Y-%m-%d-%H-%M')
+now_legivel = now.strftime('%d/%m/%Y - %Hh%Mmin.')
+arquivo_resultado = f'resultado-{now_formatado}.txt'
 
-# Percorre a lista de investigados e faz a consulta por partes
-for investigado in investigados:
-    response = consulta_por_parte(investigado)
-    if (response.status_code == 200):
-        soup = BeautifulSoup(response.content, 'html.parser')
-        if (soup.find('div', {'id':'spwTabelaMensagem'})):
-            print('Encontrei uma mensagem')
+# Abre o arquivo para gravar o resultado da consulta
+with open(arquivo_resultado, 'w', encoding='utf-8') as f:
+    f.write(f'RESULTADO DA VARREDURA REALIZADA EM {now_legivel}\n')
+    f.write('\n\nPESQUISA POR INVESTIGADOS\n\n')
+
+    # Percorre a lista de investigados e faz a consulta
+    for investigado in investigados:
+        response = consulta_por_parte(investigado)
+       
+        if (response.status_code == 200):
+            soup = BeautifulSoup(response.content, 'html.parser')
+            f.write(f'{investigado}:\n{faz_busca(soup)}')
+            f.write('\n' + '-'* 58 + '\n')
+            
         else:
-            if (soup.find('div', {'id': 'listagemDeProcessos'})):
-                print('Encontrei a listagem de processos')
-
-        
-    else:
-        with open(arquivo_resultado, 'a') as f:
-            f.write(f'{investigado}: ')
-            f.write('sem resposta do servidor. Verifique!')
+            f.write(f'{investigado}:\n')
+            f.write('Sem resposta do servidor. Verifique!')
             f.write('\n' + '-'* 58 + '\n')
 
+    # Percorre a lista de advogados e faz a consulta
+    f.write('\n\nPESQUISA POR OAB\n\n')
+    for registro in oab:
+        response = consulta_num_oab(registro)
+        if (response.status_code == 200):
+            soup = BeautifulSoup(response.content, 'html.parser')
 
+            f.write(f'{registro}:\n{faz_busca(soup)}')
+            f.write('\n' + '-'* 58 + '\n')
+                
+        else:
+           f.write(f'{registro}:\n')
+           f.write('Sem resposta do servidor. Verifique!')
+           f.write('\n' + '-'* 58 + '\n')
 
+print('Programa concluído!')
 
-
-# Verifica o "response" e grava o alerta em arquivo texto para != 200
-
-# Cria o objeto BeautifulSoup a partir do conteúdo HTML da resposta
-# soup = BeautifulSoup(response.content, 'html.parser')
-
-
-
-# Grava o resultado da pesquisa (Não foram encontrados..., Muitos processos..., Único processo, Relação de processos)
-# Percorre a lista de OAB e faz a consulta por advogado
-# Testa o "response" e grava o alerta em arquivo texto para != 200
-# Grava o resultado da pesquisa (verificar opções / testar tb com nomes)
+# Tarefas
+# Readme
 # Transformar em executável para a distribuição (não usar a opção de arquivo único)
 
 """ 
+# Encontro de vários processos na primeira página
+listagem_de_processos = conteudo.find('div', {'id': 'listagemDeProcessos'})
 
-with open('resultado.txt', 'w') as arquivo:
-    for linha in linhas:
-        conteudo = search_parte(linha)
-        print(linha, file=arquivo)
-        # Alerta: não foram encontrados processos ou foram encontrados muitos processos
-        try:
-            listagem_de_processos = conteudo.find('div', {'id':'spwTabelaMensagem'})
-            print(listagem_de_processos.get_text().strip(), file=arquivo)
-        except:
-            # Encontro de um único processo (o eSAJ já abre na página correspondente)
-            try:
-                listagem_de_processos = conteudo.find('div', {'class':'unj-entity-header__summary__barra'}) 
-                print(listagem_de_processos.get_text().strip(), file=arquivo)
-            except:
-                try:
-                        # Encontro de vários processos na primeira página
-                        listagem_de_processos = conteudo.find('div', {'id': 'listagemDeProcessos'})
+# Encontra todas as tags filhas da tag `listagem_de_processos`
+n_processo = listagem_de_processos.find('a', {'class':'linkProcesso'})
+classe = listagem_de_processos.find('div', {'class':'classeProcesso'})
+assunto = listagem_de_processos.find('div', {'class':'assuntoProcesso'})
+data_local= listagem_de_processos.find('div', {'class':'dataLocalDistribuicao'})
 
-                        # Encontra todas as tags filhas da tag `listagem_de_processos`
-                        n_processo = listagem_de_processos.find('a', {'class':'linkProcesso'})
-                        classe = listagem_de_processos.find('div', {'class':'classeProcesso'})
-                        assunto = listagem_de_processos.find('div', {'class':'assuntoProcesso'})
-                        data_local= listagem_de_processos.find('div', {'class':'dataLocalDistribuicao'})
-
-                        print(n_processo.get_text().strip(), file=arquivo)
-                        print(classe.get_text().strip(), file=arquivo)
-                        print(assunto.get_text().strip(), file=arquivo)
-                        print(data_local.get_text().strip(), file=arquivo)
-                except:
-                        print(f'Verifique manualmente o nome informado\n', file=arquivo)
-        print('*'*30, file=arquivo)
-
-print('Programa concluído!') """
+print(n_processo.get_text().strip(), file=arquivo)
+print(classe.get_text().strip(), file=arquivo)
+print(assunto.get_text().strip(), file=arquivo)
+print(data_local.get_text().strip(), file=arquivo)
+            
+"""
